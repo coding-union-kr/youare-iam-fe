296497d0da2f058c4a81699fd5f02c46c16fc040
biyam <b1y9mn@gmail.com>
2023-12-05 20:15:28 +0900

    feat: props에 handleAction 추가 (#2)

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,46 +1,55 @@
 import { ComponentProps, useRef } from 'react';
 
 // Modal 컴포넌트 사용 예시
-// <Modal width="w-80" actionText="답변 작성하러 가기" cancelText="되돌아가기">아직 답변을 등록하지 않았어요.</Modal>
+// <Modal width="w-80" actionText="답변 작성하러 가기" handleAction={이벤트핸들러} cancelText="되돌아가기">아직 답변을 등록하지 않았어요.</Modal>
 
 type ModalProps = ComponentProps<'div'> & {
   actionText: string;
   cancelText: string;
   width: string;
+  handleAction: () => void;
 };
 
-export default function Modal({ width, actionText, cancelText, children}: ModalProps) {
+export default function Modal({
+  width,
+  actionText,
+  cancelText,
+  handleAction,
+  children,
+}: ModalProps) {
   const modalRef = useRef<HTMLDialogElement>(null);
 
   const handleModalClose = () => {
-    modalRef.current?.close()
-  }
+    modalRef.current?.close();
+  };
 
   const handleModalOpen = () => {
-    modalRef.current?.showModal()
-  }
-
-  const handleAction = () => {
-    modalRef.current?.close()
-    // 답변 작성 액션 or 수정 액션
-  }
+    modalRef.current?.showModal();
+  };
 
   return (
     <>
       <div className="w-full h-full bg-secondary" onClick={handleModalOpen}>
-      🔒 질문 
+        🔒 질문
       </div>
       <dialog ref={modalRef} className="modal">
         <div className={`modal-box ${width}`}>
           <p className="py-4">{children}</p>
           <div className="flex flex-row justify-between">
-          <button className="btn w-28 btn-secondary" onClick={handleModalClose}>{cancelText}</button>
-          <button className="btn w-28 btn-primary" onClick={handleAction}>{actionText}</button>
+            <button
+              className="btn w-28 btn-secondary"
+              onClick={handleModalClose}
+            >
+              {cancelText}
+            </button>
+            <button className="btn w-28 btn-primary" onClick={handleAction}>
+              {actionText}
+            </button>
           </div>
         </div>
         <form method="dialog" className="modal-backdrop">
           <button>close</button>
-        </form>        
+        </form>
       </dialog>
     </>
   );
