b22958b7951a1fa07d335bbb400076c8df5d6a4e
seul-dev <seul.dev@gmail.com>
2023-12-07 14:36:23 +0900

    feat: Button 컴포넌트 #12 (#13)
    * feat: Button 컴포넌트 #12
    
    * feat: Button prop에 className추가
    
    * feat: KakaoLogin Button 구현 #12
    
    * style: type import 수정

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,36 @@
+import type { ComponentProps } from 'react';
+
+type ButtonProps = ComponentProps<'button'> & {
+  variant: 'primary' | 'secondary' | 'accent';
+  size?: 'xs' | 'sm' | 'normal' | 'wide';
+};
+
+export default function Button({
+  variant,
+  size = 'normal',
+  children,
+  className,
+  ...props
+}: ButtonProps) {
+  const colorVariants = {
+    primary: 'btn-primary',
+    secondary: 'btn-secondary',
+    accent: 'btn-accent',
+  };
+
+  const sizeVariants = {
+    xs: 'btn-xs',
+    sm: 'btn-sm',
+    normal: 'btn-normal',
+    wide: 'btn-wide block w-10/12 mx-auto',
+  };
+
+  return (
+    <button
+      className={`btn ${colorVariants[variant]} ${sizeVariants[size]} no-animation ${className}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
