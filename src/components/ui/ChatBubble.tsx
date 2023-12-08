a07d617aff7d6f789f3a35a976b615a650398475
biyam <b1y9mn@gmail.com>
2023-12-08 16:20:20 +0900

    feat: chat bubble 컴포넌트 구현 (#5)

---
+++ src/components/ui/ChatBubble.tsx
@@ -0,0 +1,17 @@
+const ChatBubble = ({ answer }) => {
+  console.log({ answer });
+  return (
+    <>
+      <div className="chat chat-start">
+        <div className="chat-bubble bg-[#ffffff]">{answer[0].answer}</div>
+        <div className="chat-footer opacity-50">{answer[0].createdAt}</div>
+      </div>
+      <div className="chat chat-end">
+        <div className="chat-bubble bg-[#FEB2B2]">{answer[1].answer}</div>
+        <div className="chat-footer opacity-50">{answer[1].createdAt}</div>
+      </div>
+    </>
+  );
+};
+
+export default ChatBubble;
