e3acc8ee74e8ee4a466e57808885730b3832ad2b
biyam <b1y9mn@gmail.com>
2023-12-08 16:21:05 +0900

    feat: QuestionBar 컴포넌트에 ChatBubble 컴포넌트 추가 (#3)

--- src/components/ui/QuestionBar.tsx
+++ src/components/ui/QuestionBar.tsx
@@ -1,3 +1,5 @@
+import ChatBubble from './ChatBubble';
+
 type QuestionItemType = {
   selectQuestionId: number;
   question: string;
@@ -16,37 +18,52 @@ type QuestionItemType = {
 const QuestionBar = ({ questionItem }: QuestionItemType) => {
   if (questionItem.answerCount === 0) {
     return (
-      <div className="p-1 font-neo w-[95%] h-full rounded-md border-solid border-2 border-[#4F4F4F] bg-[#E7E7E7] flex items-center">
-        <div>🔒 {questionItem.question}</div>
+      <div className=" m-2 p-2 font-neo  h-full rounded-md border-solid border-2 border-[#4F4F4F] bg-[#E7E7E7] flex items-center">
+        <div className="pl-1  pr-2">🔒</div>
+        <div>{questionItem.question}</div>
       </div>
     );
   }
 
   if (questionItem.answerCount === 1) {
     return (
-      <>
-        <div className="p-1 font-neo w-[95%] h-full rounded-md border-solid border-2 border-[#4F4F4F] bg-[#E7E7E7] flex items-center">
-          <div>🔒 {questionItem.question}</div>
+      <div className="m-2">
+        <div className="p-2 font-neo  h-full rounded-md border-solid border-2 border-[#4F4F4F] bg-[#E7E7E7] flex items-center">
+          <div className="pl-1 pr-2">🔒</div>
+          <div>{questionItem.question}</div>
         </div>
         {questionItem.isMyAnswer ? (
-          <>내 버블(답변을 두 사람 모두 등록해야 확인할 수 있어요)</>
+          <div className="chat chat-end">
+            <div className="chat-bubble bg-[#FEB2B2]">
+              답변을 두 사람 모두 등록해야 확인할 수 있어요.
+            </div>
+            <div className="chat-footer opacity-50">
+              {questionItem.createdAt}
+            </div>
+          </div>
         ) : (
-          <>상대 버블(답변을 두 사람 모두 등록해야 확인할 수 있어요)</>
+          <div className="chat chat-start">
+            <div className="chat-bubble bg-[#ffffff]">
+              답변을 두 사람 모두 등록해야 확인할 수 있어요.
+            </div>
+            <div className="chat-footer opacity-50">
+              {questionItem.createdAt}
+            </div>
+          </div>
         )}
-      </>
+      </div>
     );
   }
 
   if (questionItem.answerCount === 2) {
     return (
-      <>
-        <div className="p-1 font-neo w-[95%] h-full  rounded-md border-solid border-2 border-[#FF6666]  bg-[#FFE8E8] flex items-center">
-          🔑 {questionItem.question}
+      <div className="m-2">
+        <div className="p-2 font-neo h-full  rounded-md border-solid border-2 border-[#FF6666]  bg-[#FFE8E8] flex items-center">
+          <div className="pl-1  pr-2">🔑</div>
+          <div>{questionItem.question}</div>
         </div>
-        {questionItem.answers?.map((answer, answerIndex) => (
-          <div key={answerIndex}>{answer.answer}</div>
-        ))}
-      </>
+        <ChatBubble answer={questionItem.answer} />
+      </div>
     );
   }
 };
