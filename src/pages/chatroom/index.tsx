8a29f914319e4924b1c7cbaae01676aaa1a4f30a
seul-dev <seul.dev@gmail.com>
2023-12-08 14:19:13 +0900

    Feature/bottom nav (#15)
    * feat: Icon 컴포넌트 구현
    
    * feat: BottomNavigation 구현
    
    * feat: main page들에 BottomNavigation적용

---
+++ src/pages/chatroom/index.tsx
@@ -0,0 +1,10 @@
+import BottomNavigation from '@/components/layout/BottomNavigation';
+
+export default function Page() {
+  return (
+    <div>
+      chatroom
+      <BottomNavigation />
+    </div>
+  );
+}
