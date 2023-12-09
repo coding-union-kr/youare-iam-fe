974d0a30c56a3faabfc9dadf60829ef609c345a2
biyam <b1y9mn@gmail.com>
2023-12-09 17:36:11 +0900

    feat: 질문바 onClick 이벤트 핸들러 추가
    - 답변 1개 이하로 달린 질문바 클릭시 모달 생성
    - 답변 2개 달린 질문바 클릭시 무반응

--- src/pages/chatroom/index.tsx
+++ src/pages/chatroom/index.tsx
@@ -1,5 +1,7 @@
 import BottomNavigation from '@/components/layout/BottomNavigation';
+import Modal from '@/components/ui/Modal';
 import QuestionBar from '@/components/ui/QuestionBar';
+import { useState, useEffect } from 'react';
 
 const dummyQuestions = [
   {
@@ -13,8 +15,7 @@ const dummyQuestions = [
   {
     isRegisterQuestion: false,
     selectQuestionId: 2,
-    question:
-      '같이 해보고 싶은 버킷리스트가 있나요? 레이아웃체크를위한더미텍스트 레이아웃체크를위한더미텍스트',
+    question: '같이 해보고 싶은 버킷리스트가 있나요? ',
     createdAt: 231225,
     answerCount: 1,
     isMyAnswer: true,
@@ -22,8 +23,7 @@ const dummyQuestions = [
   {
     isRegisterQuestion: false,
     selectQuestionId: 3,
-    question:
-      '다툼이 생겼을 때 절대 안 했으면 하는 것이 있다면 무엇인가요? 레이아웃체크를위한더미텍스트 레이아웃체크를위한더미텍스트',
+    question: '다툼이 생겼을 때 절대 안 했으면 하는 것이 있다면 무엇인가요? ',
     createdAt: 231226,
     answerCount: 2,
     answer: [
@@ -44,8 +44,7 @@ const dummyQuestions = [
   {
     isRegisterQuestion: true,
     selectQuestionId: 4,
-    question:
-      '저를 만나는 이유가 뭔가요? 레이아웃체크를위한더미텍스트 레이아웃체크를위한더미텍스트',
+    question: '상대방의 어떤 점이 사랑스러운가요? ',
     createdAt: 231227,
     answerCount: 0,
   },
@@ -66,11 +65,84 @@ type QuestionItemType = {
   }[];
 };
 
+type ModalInfo = {
+  actionText: string;
+  cancelText: string;
+  bodyText: string;
+  handleAction: () => void;
+};
+
 export default function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalInfo, setModalInfo] = useState<ModalInfo>({
+    actionText: '',
+    cancelText: '',
+    bodyText: '',
+    handleAction: () => {},
+  });
+
+  const handleQuestionBarClick = ({
+    questionItem,
+  }: {
+    questionItem: QuestionItemType;
+  }) => {
+    if (questionItem.answerCount === 0) {
+      setModalInfo({
+        actionText: '답변 작성하러 가기',
+        cancelText: '되돌아가기',
+        bodyText: '둘의 답변을 기다리고 있어요. 먼저 답변을 작성해볼까요?',
+        handleAction: () => {
+          console.log('답변 작성하러 가기 action');
+        },
+      });
+      setIsModalOpen(true);
+    } else if (questionItem.answerCount === 2) {
+      setIsModalOpen(false);
+    } else if (questionItem.answerCount === 1) {
+      if (questionItem.isMyAnswer) {
+        setModalInfo({
+          actionText: '답변 작성하러 가기',
+          cancelText: '되돌아가기',
+          bodyText: '아직 답변을 등록하지 않았어요. 답변을 등록하러 가볼까요?',
+          handleAction: () => {
+            console.log('답변 작성하러 가기 action');
+          },
+        });
+      } else {
+        setModalInfo({
+          actionText: '되돌아가기',
+          cancelText: '수정하기',
+          bodyText:
+            '상대가 답변을 등록하지 않았어요. 기존 답변을 수정하시겠어요?',
+          handleAction: () => {
+            console.log('수정하기 action');
+          },
+        });
+      }
+      setIsModalOpen(true);
+    }
+  };
+
+  useEffect(() => {
+    console.log('isModalOpen', isModalOpen);
+  }, [isModalOpen]);
   return (
     <>
+      {isModalOpen && (
+        <Modal
+          modalInfo={modalInfo}
+          setIsModalOpen={setIsModalOpen}
+          isModalOpen={isModalOpen}
+        />
+      )}
       {dummyQuestions.map((questionItem: QuestionItemType, index: number) => {
-        return <QuestionBar key={index} questionItem={questionItem} />;
+        return (
+          <QuestionBar
+            key={index}
+            questionItem={questionItem}
+            onClick={() => handleQuestionBarClick({ questionItem })}
+          />
+        );
       })}
       <BottomNavigation />
     </>
