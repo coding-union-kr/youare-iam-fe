d1e2a01e2a1659a1fa1a08e0faea31f0576b001f
seul-dev <seul.dev@gmail.com>
2023-12-11 17:06:11 +0900

    Feature/service main (#32)
    * rename: component 이름 수정
    
    * feat: BasicLayout 구현 수정(#27)
    
    * feat: 서비스 메인화면 구현(#27)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,33 @@
-import React from 'react';
-export default function Home() {
-  return <></>;
-}
+import type { NextPageWithLayout } from '@/types/page';
+import BasicLayout from '@/components/layout/BasicLayout';
+import KakaoLoginButton from '@/components/ui/KakaoLoginButton';
+import Image from 'next/image';
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <>
+      <div className="h-[80%] flex flex-col justify-center">
+        <h2 className="block mb-10 text-2xl font-bold text-center font-neo"></h2>
+        <Image
+          src="/logo.png"
+          priority
+          alt="service-logo"
+          width="250"
+          height="250"
+        />
+        <h1 className="block text-4xl font-extrabold text-center font-neo">
+          너는, 나는
+        </h1>
+      </div>
+      <KakaoLoginButton />
+    </>
+  );
+};
+
+Home.getLayout = function getLayout(page) {
+  return (
+    <BasicLayout className="bg-[#F7CBC3] justify-center">{page}</BasicLayout>
+  );
+};
+
+export default Home;
