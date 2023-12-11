8b9cc97edfe6abbc6053bec388e68f7b3961161d
biyam <101965666+biyamn@users.noreply.github.com>
2023-12-11 15:49:47 +0900

    feat: 질문 선택 페이지 추가 (#22) (#26)
    * feat: 질문 선택 페이지 추가 (#22)
    
    - mvp 목표만 구현
    
    * feat: page 레이아웃 적용

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPageWithLayout } from '@/types/page';
 import MainLayout from '@/components/layout/MainLayout';
+
 import ListItem from '@/components/ui/ListItem';
 
 const dummyQuestionList = [
@@ -11,15 +12,23 @@ const dummyQuestionList = [
     questionId: 2,
     question: '다툼이 생겼을 때 절대 안 했으면 하는 것이 있다면 무엇인가요?',
   },
+  {
+    questionId: 3,
+    question: '서로를 마음껏 자랑해보아요! 무엇이든 좋아요',
+  },
+  {
+    questionId: 4,
+    question: '산타를 몇살까지 믿었나요?',
+  },
 ];
 
 const Page: NextPageWithLayout = () => {
   return (
-    <div>
+    <div className="py-20">
       {dummyQuestionList.map((question) => (
         <div
           key={question.questionId}
-          className="flex flex-col items-center justify-center"
+          className="flex flex-col justify-center items-center"
         >
           <ListItem question={question.question} />
         </div>
