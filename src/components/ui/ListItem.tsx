349dae42120f9e8f64717ba8d190192b230bea71
biyam <101965666+biyamn@users.noreply.github.com>
2023-12-12 13:21:37 +0900

    Feature/invite page (#33)
    * feat: prop으로 받은 className을 TextArea 컴포넌트에 적용
    
    * feat: ListItem 컴포넌트 prop으로 className 추가 및 적용
    
    * feat: 초대 링크 렌딩 페이지(invite page) 구현 (#30)

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -1,10 +1,13 @@
 type ListItemProps = {
   question: string;
+  className?: string;
 };
 
-const ListItem = ({ question }: ListItemProps) => {
+const ListItem = ({ question, className }: ListItemProps) => {
   return (
-    <div className="flex flex-col justify-center border-2 border-[black] rounded-lg bg-secondary h-20 w-[80%] m-1 p-2">
+    <div
+      className={`flex flex-col justify-center border-2 border-[black] rounded-lg bg-secondary h-20 w-[80%] m-1 p-2 ${className}`}
+    >
       {question}
     </div>
   );
