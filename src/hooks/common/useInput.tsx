47d73b6a23b6fc4feb2c1c6ce74c91247bf516bc
seul-dev <seul.dev@gmail.com>
2023-12-14 11:35:15 +0900

    feat: useInput 구현 수정 - validation 추가 - hooks 경로 수정

---
+++ src/hooks/common/useInput.tsx
@@ -0,0 +1,31 @@
+import { type ChangeEventHandler, useCallback, useState } from 'react';
+
+type ReturnType = [
+  string,
+  ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>,
+  string,
+];
+
+const useInput = (
+  initialValue = '',
+  validator = (value: string) => ''
+): ReturnType => {
+  const [state, setState] = useState(initialValue);
+  const [error, setError] = useState('');
+
+  const onChange: ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement> =
+    useCallback(
+      ({ target }) => {
+        const { value } = target;
+        setState(value);
+
+        const error = validator(value);
+        setError(error);
+      },
+      [validator]
+    );
+
+  return [state, onChange, error];
+};
+
+export default useInput;
