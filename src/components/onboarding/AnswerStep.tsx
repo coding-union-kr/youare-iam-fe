0fb63dd3240d4c77f52ece23809c8fcd556d9aa8
seul-dev <seul.dev@gmail.com>
2023-12-14 11:08:52 +0900

    feat: AnswerStep에 답변등록 폼 추가

--- src/components/onboarding/AnswerStep.tsx
+++ src/components/onboarding/AnswerStep.tsx
@@ -1,11 +1,25 @@
 import Button from '../ui/Button';
 import type { OnboardingStepPros } from './Intro';
+import TextArea from '../ui/TextArea';
+import useInput from '../hooks/common/useInput';
+import ListItem from '../ui/ListItem';
 
 export default function AnswerStep({ onNext }: OnboardingStepPros) {
+  const [answer, onChange] = useInput('');
+
+  const handleComplete = () => {
+    //Todo: 답변 등록하기 기능 구현
+    console.log(answer);
+
+    onNext();
+  };
+
   return (
     <>
+      <ListItem question="" />
       <p>내가 먼저 답변을 작성해볼까요?</p>
-      <Button variant="primary" size="wide" onClick={onNext}>
+      <TextArea value={answer} onChange={onChange} />
+      <Button variant="primary" size="wide" onClick={handleComplete}>
         답변 등록하기
       </Button>
     </>
