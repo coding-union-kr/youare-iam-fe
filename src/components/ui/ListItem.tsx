9e82663141d6191d90161dd3ad403806fd2fe2a2
biyam <101965666+biyamn@users.noreply.github.com>
2023-12-15 16:59:06 +0900

    Feat: 질문 선택 페이지, 대화 상세 페이지 API 연동 (#49)
    * feat: 질문 선택 api 연동
    
    * feat: 질문 선택 api 연동
    
    * feat: post 요청 보내는 코드 추가
    
    * feat: chatroom으로 이동하는 라우터 설정 추가 (#39)
    
    * feat: 해시 라우팅 구현 (#39)
    
    * Merge Conflict 해결
    
    * feat: 질문 리스트 조회 api 연동(임시)
    
    - api가 나왔을 때 바로 연동할 수 있게끔 작업
    
    * feat: 모달 스타일링 및 조건 분기문 수정
    
    * feat: chatroom Index.tsx 코드와  동일하게 변경, 파일명 변경
    
    * feat: 답변 작성하기 페이지로 이동하는 설정 추가 (#40)
    
    * feat: BottomNavigation 컴포넌트 z-index 변경
    
    * style: chatroom 컴포넌트에 margin bottom 속성 부여
    
    * fix: actionText와 cancelText 내용 뒤바꾸기

--- src/components/ui/ListItem.tsx
+++ src/components/ui/ListItem.tsx
@@ -1,11 +1,13 @@
 type ListItemProps = {
   question: string;
   className?: string;
+  onClick: () => void;
 };
 
-const ListItem = ({ question, className }: ListItemProps) => {
+const ListItem = ({ question, className, onClick }: ListItemProps) => {
   return (
     <div
+      onClick={onClick}
       className={`flex flex-col justify-center border-2 border-[black] rounded-lg bg-secondary h-20 w-[80%] m-1 p-2 ${className}`}
     >
       {question}
--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,36 +1,61 @@
+import axios from 'axios';
 import type { NextPageWithLayout } from '@/types/page';
 import MainLayout from '@/components/layout/MainLayout';
-
 import ListItem from '@/components/ui/ListItem';
+import { useRouter } from 'next/router';
 
-const dummyQuestionList = [
-  {
-    questionId: 1,
-    question: '같이 해보고 싶은 버킷리스트가 있나요?',
-  },
-  {
-    questionId: 2,
-    question: '다툼이 생겼을 때 절대 안 했으면 하는 것이 있다면 무엇인가요?',
-  },
-  {
-    questionId: 3,
-    question: '서로를 마음껏 자랑해보아요! 무엇이든 좋아요',
-  },
-  {
-    questionId: 4,
-    question: '산타를 몇살까지 믿었나요?',
-  },
-];
+const mockServerURL =
+  'https://cc7831bd-6881-44ff-9534-f344d05bc5ad.mock.pstmn.io';
+const path = '/api/v1/questions';
+const apiEndpoint = `${mockServerURL}${path}`;
 
-const Page: NextPageWithLayout = () => {
+type Questions = {
+  questions: Question[];
+};
+
+type Question = {
+  questionId: number;
+  question: string;
+};
+
+const Page: NextPageWithLayout<Questions> = ({ questions }) => {
+  const router = useRouter();
+
+  const handleItemClick = async (questionId: Question['questionId']) => {
+    console.log(questionId);
+    try {
+      // POST 요청으로 questionId를 보내기
+      const postResponse = await axios.post(apiEndpoint, {
+        questionId: questionId,
+      });
+
+      // 응답으로 받은 selectedQuestionId를 가져오기
+      const selectedQuestionId = postResponse.data.selectedQuestionId;
+      console.log(selectedQuestionId);
+
+      const targetPath = '/chatroom';
+      // selectedQuestionId가 있는 위치로 이동하기
+      // 위치 만들기
+      router.push({
+        pathname: targetPath,
+        hash: '10', // 원래 selectedQuestionId가 들어가야 함. 임시로 큰 값을 넣어봄
+      });
+    } catch (error) {
+      console.error('Error fetching data:', (error as Error).message);
+    }
+  };
+
   return (
     <div className="py-20">
-      {dummyQuestionList.map((question) => (
+      {questions.map((question) => (
         <div
           key={question.questionId}
           className="flex flex-col justify-center items-center"
         >
-          <ListItem question={question.question} />
+          <ListItem
+            question={question.question}
+            onClick={() => handleItemClick(question.questionId)}
+          />
         </div>
       ))}
     </div>
@@ -39,6 +64,18 @@ const Page: NextPageWithLayout = () => {
 
 Page.getLayout = function getLayout(page) {
   return <MainLayout>{page}</MainLayout>;
+};
+
+export const getStaticProps = async () => {
+  try {
+    const response = await axios.get(apiEndpoint);
+    const questions = response.data;
+
+    return { props: { questions } };
+  } catch (error) {
+    console.error('Error fetching data:', (error as Error).message);
+    return { props: { questions: [] } };
+  }
 };
 
 export default Page;
