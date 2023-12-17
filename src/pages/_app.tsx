b9757dab5d8e078ba4fa0fbd6eef0631c27e4d5a
biyam <b1y9mn@gmail.com>
2023-12-17 13:31:38 +0900

    feat: queryClient 선언 및 app에 적용

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NextPageWithLayout } from '@/types/page';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 }
