9416b66d45a26b181321a8104edc8ef80f9697c6
seul-dev <seul.dev@gmail.com>
2023-12-18 14:15:32 +0900

    Feature/auth (#53)
    * chore: .gitignore에 .env 추가
    
    * feat: 카카오 로그인 페이지로 리다이렉트
    
    * feat: login 기본 동작 구현
    
    * rename: kakoAuth 관련 url 상수 디렉토리로 분리
    
    * feat: 로그인 시도한 페이지로 리다이렉트하는 기능 추가
    
    * feat: 에러메시지 추가

--- src/components/ui/KakaoLoginButton.tsx
+++ src/components/ui/KakaoLoginButton.tsx
@@ -1,20 +1,24 @@
 import type { ComponentProps } from 'react';
 import Button from './Button';
 import KakaoIcon from '@/components/icons/KakaoIcon';
+import Link from 'next/link';
+import { KAKAO_AUTH_URL } from '@/constants/kakaoAuth';
 
 type ButtonProps = ComponentProps<'button'>;
 
 export default function KakaoLoginButton({ ...props }: ButtonProps) {
   const kakaoStyle = `bg-[#FEE400] hover:bg-[#FEE400] border-[#FEE400] hover:border-[#FEE400] text-[#371C1D]`;
   return (
-    <Button
-      className={`${kakaoStyle} flex items-center justify-center`}
-      size="wide"
-      variant="primary"
-      {...props}
-    >
-      <KakaoIcon className="w-6 h-6" />
-      카카오 로그인
-    </Button>
+    <Link href={KAKAO_AUTH_URL} className="w-full">
+      <Button
+        className={`${kakaoStyle} flex items-center justify-center`}
+        size="wide"
+        variant="primary"
+        {...props}
+      >
+        <KakaoIcon className="w-6 h-6" />
+        카카오 로그인
+      </Button>
+    </Link>
   );
 }
