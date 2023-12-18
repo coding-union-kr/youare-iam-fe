fbea8ea476dc8960d560b28c90fe60dc3e026a86
seul-dev <seul.dev@gmail.com>
2023-12-18 17:00:29 +0900

    feat: useInput 구현 수정 - onChange에 추가되어야하는 로직을 callback으로 전달할 수 있도록 수정

--- src/components/onboarding/AnswerStep.tsx
+++ src/components/onboarding/AnswerStep.tsx
@@ -1,12 +1,24 @@
+import { useRecoilState } from 'recoil';
 import type { OnboardingStepPros } from './Intro';
-import ListItem from '../ui/ListItem';
 import useInput from '@/hooks/common/useInput';
+import { onboardingState } from '@/store/onboardingState';
 import AnswerForm from '../answer/AnswerForm';
 import QuestionTitle from '../answer/QuestionTitle';
 
 export default function AnswerStep({ onNext }: OnboardingStepPros) {
-  const [answer, onChange, errorMessage] = useInput('', (value) =>
-    value.trim() ? '' : '답변을 입력해주세요'
+  const [onboardingData, setOnboardingData] = useRecoilState(onboardingState);
+
+  const onChangeCallback = (newAnswer: string) => {
+    setOnboardingData((prevState) => ({
+      ...prevState,
+      answer: newAnswer,
+    }));
+  };
+
+  const [answer, onChange, errorMessage] = useInput(
+    onboardingData.answer,
+    (value) => (value.trim() ? '' : '답변을 입력해주세요'),
+    onChangeCallback
   );
 
   const handleSubmit = (e: React.FormEvent) => {
@@ -16,15 +28,12 @@ export default function AnswerStep({ onNext }: OnboardingStepPros) {
       return;
     }
 
-    //Todo: 답변 등록하기 기능 구현
-    console.log(answer);
-
-    // main action : onboarding 데이터로 저장하기
     onNext();
   };
 
   return (
     <>
+      {/* Todo: question 업데이트하기 */}
       <QuestionTitle question="당신이 좋아하는 계절은 언제인가요?" />
       <p>내가 먼저 답변을 작성해볼까요?</p>
       <AnswerForm
