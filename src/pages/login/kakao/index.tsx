7f5143c0d476f2a42c7e123b5bbf99b54a40dcd7
seul-dev <seul.dev@gmail.com>
2023-12-18 14:15:32 +0900

    Feature/auth (#53)
    * chore: .gitignore에 .env 추가
    
    * feat: 카카오 로그인 페이지로 리다이렉트
    
    * feat: login 기본 동작 구현
    
    * rename: kakoAuth 관련 url 상수 디렉토리로 분리
    
    * feat: 로그인 시도한 페이지로 리다이렉트하는 기능 추가
    
    * feat: 에러메시지 추가

---
+++ src/pages/login/kakao/index.tsx
@@ -0,0 +1,38 @@
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import { LOCAL_STORAGE_KEYS } from '@/constants/localStorageKeys';
+
+export default function Login() {
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+  const { code } = router.query;
+
+  useEffect(() => {
+    console.log(router.asPath);
+    const fetchToken = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/members/kakao/callback?code=${code}`
+        );
+
+        localStorage.setItem(
+          LOCAL_STORAGE_KEYS.TOKEN,
+          res.headers['authorization']
+        );
+
+        const prevUrl = localStorage.getItem(LOCAL_STORAGE_KEYS.PREV_URL);
+        router.push(prevUrl || '/onboarding');
+        localStorage.removeItem(LOCAL_STORAGE_KEYS.PREV_URL);
+      } catch (err) {
+        setError('로그인 중 오류가 발생했습니다. 다시 시도해주세요.');
+      }
+    };
+
+    if (code) {
+      fetchToken();
+    }
+  }, [code, router]);
+
+  return <div>{error && <p>{error}</p>}</div>;
+}
