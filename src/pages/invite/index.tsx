4bf8ac63702287b6342edf86bc65545f476ecdbc
biyam <b1y9mn@gmail.com>
2023-12-19 19:06:41 +0900

    feat: 초대링크를 보낸 사용자 이름 데이터 받아오기

--- src/pages/invite/index.tsx
+++ src/pages/invite/index.tsx
@@ -4,15 +4,25 @@ import Image from 'next/image';
 import ListItem from '@/components/ui/ListItem';
 import TextArea from '@/components/ui/TextArea';
 import Button from '@/components/ui/Button';
+import axios from 'axios';
 
 const question = '같이 해보고 싶은 버킷리스트가 있나요?';
-const Page: NextPageWithLayout = () => {
+
+const baseURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const path = '/api/v1/members/invite/info';
+const apiEndpoint = `${baseURL}${path}`;
+
+type InviteInfo = {
+  selectedQuestionId: number;
+  invitedPersonName: string;
+};
+
+const Page: NextPageWithLayout<InviteInfo> = ({ inviteInfo }) => {
+  console.log('inviteInfo: ', inviteInfo);
   return (
     <>
       <div className="mt-10">
-        <div>
-          {'{'}memberName{'}'}이
-        </div>
+        <div>{inviteInfo?.invitedPersonName}이</div>
         <div>초대 링크를 보냈어요!</div>
       </div>
       {/* <ListItem question={question} className="mt-5 bg-white" /> */}
@@ -39,6 +49,18 @@ Page.getLayout = function getLayout(page) {
   return (
     <BasicLayout className="bg-[#F7CBC3] justify-center">{page}</BasicLayout>
   );
+};
+
+export const getStaticProps = async () => {
+  try {
+    const response = await axios.get(apiEndpoint);
+    const inviteInfo = response.data;
+    console.log('inviteInfo: ', inviteInfo);
+    return { props: { inviteInfo } };
+  } catch (error) {
+    console.error('Error fetching data:', (error as Error).message);
+    return { props: { inviteInfo: [] } };
+  }
 };
 
 export default Page;
