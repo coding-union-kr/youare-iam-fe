3cfcd0ac688a5c4395827fd289abbd57e82a06bb
seul-dev <seul.dev@gmail.com>
2023-12-19 14:06:02 +0900

    feat: axios request hearder에 accessToken추가

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -1,9 +1,24 @@
-import axios, { type AxiosResponse } from 'axios';
+import { LOCAL_STORAGE_KEYS } from '@/constants/localStorageKeys';
+import axios, { InternalAxiosRequestConfig, type AxiosResponse } from 'axios';
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   timeout: 15000,
 });
+
+const interceptorRequestFulfilled = (config: InternalAxiosRequestConfig) => {
+  if (typeof window === 'undefined') return config;
+
+  const accessToken = localStorage.getItem(LOCAL_STORAGE_KEYS.TOKEN);
+  if (!config.headers) return config;
+  if (!accessToken) return config;
+
+  config.headers.Authorization = accessToken;
+
+  return config;
+};
+
+instance.interceptors.request.use(interceptorRequestFulfilled);
 
 export const get = <T = any, R = AxiosResponse<T>>(url: string) => {
   return instance.get<T, R>(url);
