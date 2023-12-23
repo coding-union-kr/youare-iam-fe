639c1b6fea6e23a34d83fde01f4f9c9899a12621
biyam <101965666+biyamn@users.noreply.github.com>
2023-12-23 14:57:08 +0900

    Feature/404 에러 페이지  (#66)

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+const Custom404 = () => {
+  return (
+    <section>
+      <div className="h-screen flex flex-col items-center pt-20">
+        <div className="font-neo text-4xl mb-4 text-primary">
+          404 Page Not Found
+        </div>
+        <div className="font-neo text-lg mb-4">
+          원하시는 페이지를 찾을 수 없습니다.
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Custom404;
