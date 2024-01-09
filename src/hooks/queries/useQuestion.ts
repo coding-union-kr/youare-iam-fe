f57128bb1d5044a4fe7291945ba1e1650ac77ee2
seul-dev <seul.dev@gmail.com>
2024-01-09 15:10:52 +0900

    feat: answerform페이지에서 question fetching 부분을 훅으로 분리(dehydrate 추가)

---
+++ src/hooks/queries/useQuestion.ts
@@ -0,0 +1,27 @@
+import { get } from '@/libs/api';
+import { useQuery } from '@tanstack/react-query';
+
+type Question = {
+  question: string;
+};
+
+export const getQuestion = async (id: number) => {
+  const res = await get<Question>(`/api/v1/answer?selected-question-id=${id}`);
+  return res.data.question;
+};
+
+export default function useQuestion(id: number) {
+  const fallbackQuestion = '';
+
+  const {
+    data: question = fallbackQuestion,
+    error,
+    isLoading,
+    isError,
+  } = useQuery<string>({
+    queryKey: ['question', id],
+    queryFn: () => getQuestion(id),
+  });
+
+  return { question, error, isLoading, isError };
+}
