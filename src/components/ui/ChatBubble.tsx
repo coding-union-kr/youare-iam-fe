6027240a8cdd170e06eec66642ce3d1ae1893fa5
biyamn <b1y9mn@gmail.com>
2024-01-22 18:32:02 +0900

    fix: answer 옵셔널체이닝 추가

--- src/components/ui/ChatBubble.tsx
+++ src/components/ui/ChatBubble.tsx
@@ -17,12 +17,12 @@ const ChatBubble = ({ answer }: ChatBubbleProps) => {
   return (
     <>
       <div className="chat chat-start">
-        <div className="chat-bubble bg-[#ffffff]">{otherAnswer.answer}</div>
-        <div className="chat-footer opacity-50">{otherAnswer.createdAt}</div>
+        <div className="chat-bubble bg-[#ffffff]">{otherAnswer?.answer}</div>
+        <div className="chat-footer opacity-50">{otherAnswer?.createdAt}</div>
       </div>
       <div className="chat chat-end">
-        <div className="chat-bubble bg-[#FEB2B2]">{myAnswer.answer}</div>
-        <div className="chat-footer opacity-50">{myAnswer.createdAt}</div>
+        <div className="chat-bubble bg-[#FEB2B2]">{myAnswer?.answer}</div>
+        <div className="chat-footer opacity-50">{myAnswer?.createdAt}</div>
       </div>
     </>
   );
