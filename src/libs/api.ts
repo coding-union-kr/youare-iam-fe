83dcc66afba17e2414ee7e13fca29cf7a9cd6548
seul-dev <seul.dev@gmail.com>
2024-01-25 10:56:47 +0900

    feat: 리프레시 토큰 만료응답 처리 구현

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -12,6 +12,14 @@ import {
   setAuthHeader,
 } from './token';
 import { ErrorResponse } from '@/types/api';
+import { showToastErrorMessage } from '@/util/showToastErrorMessage';
+import toast from 'react-hot-toast';
+
+const ERROR_CODES = {
+  INVALID_ACCESS_TOKEN: 'AU001',
+  EXPIRED_ACCESS_TOKEN: 'AU002',
+  EXPIRED_REFRESH_TOKEN: 'AU003',
+};
 
 export const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
@@ -43,17 +51,18 @@ const interceptorResponseFulfilled = (res: AxiosResponse) => {
 const interceptorResponseRejected = async (error: AxiosError) => {
   const originalRequest = error.config;
   if (error.response?.status === 401 && originalRequest) {
-    const { code } = error.response.data as ErrorResponse;
+    const errorResponse = error.response.data as ErrorResponse;
 
     // code : AU001 : 토큰 삭제->  로그인 페이지로 이동
-    if (code === 'AU001') {
+    if (errorResponse.code === ERROR_CODES.INVALID_ACCESS_TOKEN) {
+      showToastErrorMessage(errorResponse);
       removeAccessToken();
-      window.location.href = '/login';
+      window.location.href = '/';
       return Promise.reject(error.response.data as ErrorResponse);
     }
 
     // code : AU002 - 액세스 토큰 재발급 api -> 토큰 다시 저장, 헤더에 토큰 설정 -> 요청 재시도
-    if (code === 'AU002') {
+    if (errorResponse.code === ERROR_CODES.EXPIRED_ACCESS_TOKEN) {
       refreshTokenAndRetryRequest(instance, originalRequest);
     }
 
@@ -114,27 +123,25 @@ async function refreshTokenAndRetryRequest(
     const axiosError = error as AxiosError;
     const errorResponse = axiosError.response?.data as ErrorResponse;
 
-    if (errorResponse.code === 'AU003') {
-      // 로그아웃 처리 후 로그인 페이지로 이동
-      // removeAccessToken();
-      // window.location.href = '/';
-      console.log(errorResponse);
+    if (errorResponse.code === ERROR_CODES.EXPIRED_REFRESH_TOKEN) {
+      showToastErrorMessage(errorResponse);
+
       try {
         const res = await refreshInstance.delete('/api/v1/members/auth/logout');
-
         if (res.status === 205) {
-          // removeAccessToken();
-          console.log('로그아웃 성공');
+          toast.success('로그아웃 되었습니다.');
+          removeAccessToken();
+          window.location.href = '/';
         }
       } catch (error) {
         const axiosError = error as AxiosError;
         const errorResponse = axiosError.response?.data as ErrorResponse;
-        console.log(errorResponse);
+        if (errorResponse.code === ERROR_CODES.INVALID_ACCESS_TOKEN) {
+          showToastErrorMessage(errorResponse);
+          removeAccessToken();
+          window.location.href = '/';
+        }
       }
-
-      return;
     }
-
-    console.log('error');
   }
 }
