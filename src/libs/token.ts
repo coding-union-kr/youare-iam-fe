05602f69a6f5f608cc1fe3a54b5b121752441f1b
seul-dev <seul.dev@gmail.com>
2024-01-26 19:43:08 +0900

    feat: serverside 토큰 관련 함수 구현

--- src/libs/token.ts
+++ src/libs/token.ts
@@ -1,6 +1,8 @@
 import type { AxiosInstance } from 'axios';
 import Cookies from 'js-cookie';
 import { ACCESS_TOKEN } from '@/constants/auth';
+import { GetServerSidePropsContext } from 'next';
+import { parseCookies } from 'nookies';
 
 const ACCESS_TOKEN_EXPIRES = 14; // 2주 (리프레시 토큰 만료 기간)
 const isProduction = process.env.NODE_ENV === 'production';
@@ -18,6 +20,32 @@ export const setAccessToken = (token: string) => {
 
 export const removeAccessToken = () => {
   Cookies.remove(ACCESS_TOKEN);
+};
+
+export const getServersideAccessToken = (
+  context: GetServerSidePropsContext
+) => {
+  const cookies = parseCookies(context);
+  const accessToken = cookies[ACCESS_TOKEN];
+  return accessToken;
+};
+
+export const setServersideAccessToken = (
+  context: GetServerSidePropsContext,
+  token: string
+) => {
+  const expires = 14 * 24 * 60 * 60; // 2주
+
+  context.res.setHeader(
+    'Set-Cookie',
+    `${ACCESS_TOKEN}=${token}; Path=/; Secure; Max-Age=${expires}`
+  );
+};
+
+export const removeServersideAccessToken = (
+  context: GetServerSidePropsContext
+) => {
+  context.res.setHeader('Set-Cookie', `${ACCESS_TOKEN}=; Path=/; Max-Age=0`);
 };
 
 export const setAuthHeader = (api: AxiosInstance, token: string) => {
