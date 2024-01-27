da2f396645c04d5b7d0353d310abd3b84cd7bd1d
biyamn <b1y9mn@gmail.com>
2024-01-27 19:40:24 +0900

    refactor: 유저타입, 경로 타입 추가

--- src/util/userStatusRouting.ts
+++ src/util/userStatusRouting.ts
@@ -4,9 +4,7 @@ import { ACCESS_TOKEN } from '@/constants/auth';
 import { setAuthHeader } from '@/libs/token';
 import { GetServerSidePropsContext } from 'next';
 
-type DisallowMap = {
-  [key: string]: RegExp[];
-};
+type UserStatus = 'COUPLE_USER' | 'COUPLE_WAITING_USER' | 'NON_COUPLE_USER';
 
 export const userStatusRouting = async (context: GetServerSidePropsContext) => {
   const instance = axios.create({
@@ -21,23 +19,26 @@ export const userStatusRouting = async (context: GetServerSidePropsContext) => {
   const res = await instance.get(`/api/v1/members/user-status`);
 
   const path = context.resolvedUrl;
-  const disallowMap: DisallowMap = {
-    COUPLE_USER: [
-      /\/onboarding/,
-      /\/invite\/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/,
-    ],
-    COUPLE_WAITING_USER: [
-      /\/onboarding/,
-      /\/invite\/[0-9a-z]{8}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{4}-[0-9a-z]{12}/,
-      /\/answer\/\d*/,
-    ],
-    NON_COUPLE_USER: [/\/answer\/\d*/, /\/questions/], // checkAuth 때문에 동작하지 않음
+
+  const pathPatterns = {
+    onboarding: /^\/onboarding$/,
+    invite: /^\/invite\/[\da-z]{8}(-[\da-z]{4}){3}-[0-9a-z]{12}$/,
+    answer: /^\/answer\/\d*$/,
+    questions: /^\/questions$/,
   };
 
-  if (
-    disallowMap[res.data.userStatus]?.some((regex: RegExp) => regex.test(path))
-  ) {
-    console.log('리다이렉트5');
+  const disallowedPaths: Record<UserStatus, RegExp[]> = {
+    COUPLE_USER: [pathPatterns.onboarding, pathPatterns.invite],
+    COUPLE_WAITING_USER: [
+      pathPatterns.onboarding,
+      pathPatterns.questions,
+      pathPatterns.answer,
+    ],
+    NON_COUPLE_USER: [pathPatterns.questions, pathPatterns.answer], // checkAuth 때문에 동작하지 않음
+  };
+
+  const userStatus = res.data.userStatus as UserStatus;
+  if (disallowedPaths[userStatus]?.some((pattern) => pattern.test(path))) {
     return {
       redirect: {
         permanent: false,
