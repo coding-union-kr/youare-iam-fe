8d1ba83c5486cd6fad46dca393e2b4e716c34d3e
biyam <101965666+biyamn@users.noreply.github.com>
2024-01-28 23:28:29 +0900

    Feature/routing based on user status (#95)
    * feat: 사용자 상태 api 연결 테스트
    
    * feat: userStatus에 따라 세 경로로 라우팅
    
    * feat: 유저 상태에 따른 라우팅 구현
    
    - userStatusRouting 함수 추가 및 적용
    
    * chore: 주석 삭제
    
    * chore: TODO 주석 추가
    
    * feat: 정규표현식 추가 및 적용
    
    * refactor: 모든 경로를 정규표현식으로 표현한 disallowMap 추가
    
    * refactor: 유저타입, 경로 타입 추가
    
    * rename: 파일명 변경
    
    - userStatusRouting => disallowAccess
    
    * feat: 404 페이지에 '홈으로 돌아가기' 버튼 추가
    
    * feat: 유저 상태 타입 추가
    
    * feat: serversideApi 적용

--- src/types/api.ts
+++ src/types/api.ts
@@ -8,3 +8,13 @@ export type Question = {
   questionId: number;
   question: string;
 };
+
+export type UserStatus =
+  | 'COUPLE_USER'
+  | 'COUPLE_WAITING_USER'
+  | 'NON_COUPLE_USER';
+
+export type UserData = {
+  userStatus: UserStatus;
+  linkKey: string;
+};
