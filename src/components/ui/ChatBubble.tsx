7af32a00e9786aedf5dde28de740849e66803120
biyam <101965666+biyamn@users.noreply.github.com>
2024-01-30 18:10:24 +0900

    feat: 날짜 형식 변경하는 코드 추가 (#98)

--- src/components/ui/ChatBubble.tsx
+++ src/components/ui/ChatBubble.tsx
@@ -1,5 +1,6 @@
 import { useRecoilValue } from 'recoil';
 import { myIdState } from '@/store/myIdState';
+import { applyDateFormatting } from '@/util/applyDateFormatting';
 
 type ChatBubbleProps = {
   answer: {
@@ -12,21 +13,28 @@ type ChatBubbleProps = {
 
 const ChatBubble = ({ answer }: ChatBubbleProps) => {
   const myId = useRecoilValue(myIdState);
+
   const myAnswer = answer.find((answer) => answer.memberId === myId);
   const otherAnswer = answer.find((answer) => answer.memberId !== myId);
+
+  const myDate = myAnswer ? applyDateFormatting(myAnswer.createdAt) : null;
+  const otherDate = otherAnswer
+    ? applyDateFormatting(otherAnswer.createdAt)
+    : null;
+
   return (
     <>
       <div className="chat chat-start">
         <div className="chat-bubble bg-[#ffffff] text-neutral-500 flex items-center">
           {otherAnswer?.answer}
         </div>
-        <div className="chat-footer opacity-50 ">{otherAnswer?.createdAt}</div>
+        <div className="chat-footer opacity-50 ">{otherDate}</div>
       </div>
       <div className="chat chat-end">
         <div className="chat-bubble bg-[#FEB2B2] text-neutral-500 flex items-center">
           {myAnswer?.answer}
         </div>
-        <div className="chat-footer opacity-50">{myAnswer?.createdAt}</div>
+        <div className="chat-footer opacity-50">{myDate}</div>
       </div>
     </>
   );
