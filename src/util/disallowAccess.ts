4210b43820be497d26b2bb8d5d6c382d3fae9398
biyamn <b1y9mn@gmail.com>
2024-02-02 17:35:29 +0900

    chore: 주석 추가

--- src/util/disallowAccess.ts
+++ src/util/disallowAccess.ts
@@ -43,8 +43,9 @@ export const disallowAccess = async (context: GetServerSidePropsContext) => {
       };
     }
   } catch (error: unknown) {
-    // fetchData가 실패했는데, 로그인이 안돼서 실패한 경우일 때 페이지 접근을 막지 않음(로그인이 안된 상태에서 /invite 접근 가능)
-    // /invite에는 authCheck가 없기 때문에 아래의 코드가 필요함
+    // fetchData가 실패했는데, 로그인이 안돼서 실패한 경우일 때 페이지 접근을 막지 않음(로그인이 안된 상태에서 /invite 접근 가능하도록)
+    // /invite에는 authCheck가 없기 때문에 아래의 코드가 필요함.
+    // 다른 페이지는 authCheck를 하기 때문에 로그인을 안했으면 /로 redirect되어 아래 코드가 실행되지 않음
     const errorResponse = error as ErrorResponse;
     if (errorResponse.status === 401 && errorResponse.code === 'AU001') {
       return;
