768a7427741696492c569c0c2b3260e781b10650
seul-dev <seul.dev@gmail.com>
2024-03-14 17:08:28 +0900

    fix: 빌드 후 progressBar 업데이트 되지 않는 문제 수정 - tailwind에서 동적으로 생성되는 클래스가 적용되지 않는 문제

--- src/components/onboarding/OnboardingStepNavigator.tsx
+++ src/components/onboarding/OnboardingStepNavigator.tsx
@@ -1,4 +1,3 @@
-import { onboardingSteps } from '@/pages/onboarding';
 import LeftArrow from '../icons/LeftArrow';
 
 type Props = {
@@ -10,13 +9,13 @@ export default function OnboardingStepNavigator({
   currentStepIndex,
   onPrev,
 }: Props) {
-  const progress = Math.floor(((currentStepIndex + 1) / 3) * 100);
+  const progressBarWidth = ['w-1/3', 'w-2/3', 'w-full'][currentStepIndex];
 
   return (
     <div className="flex items-center justify-center w-full">
-      {!!currentStepIndex && <LeftArrow className="mr-5" onClick={onPrev} />}
+      {currentStepIndex > 0 && <LeftArrow className="mr-5" onClick={onPrev} />}
       <div className="w-[80%] h-2 rounded-lg bg-gray-light">
-        <div className={`h-full w-[${progress}%] bg-primary rounded-lg`} />
+        <div className={`h-full ${progressBarWidth} bg-primary rounded-lg`} />
       </div>
     </div>
   );
