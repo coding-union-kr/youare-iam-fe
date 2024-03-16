4fdbce0e7d20435a381615ecade7f51822cc7486
seul-dev <seul.dev@gmail.com>
2024-03-16 22:01:37 +0900

    HOTFIX: 토큰 재발급 응답코드 수정

--- src/libs/serversideApi.ts
+++ src/libs/serversideApi.ts
@@ -93,7 +93,7 @@ async function refreshTokenAndRetryRequest(
   try {
     const res = await refreshInstance.post('/api/v1/members/auth/token');
 
-    if (res.status === 201) {
+    if (res.status === 200) {
       const accessToken = res.headers['authorization'].split(' ')[1];
 
       setServersideAccessToken(context, accessToken);
