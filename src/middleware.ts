674bcb7fd02e956c2e71bbbc7db5b53cdce1ee3d
seul-dev <seul.dev@gmail.com>
2024-03-24 18:17:02 +0900

    feat: userStatus fetching 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { ACCESS_TOKEN, USER_STATUS } from './constants/cookie';
 
 import { COUPLE_WAITING_USER, NON_COUPLE_USER } from './constants/userStatus';
 
-export function middleware(req: NextRequest, res: NextResponse) {
+export async function middleware(req: NextRequest, res: NextResponse) {
   // checkAuth
   const accessToken = req.cookies.get(ACCESS_TOKEN)?.value;
   if (!accessToken) {
@@ -17,15 +17,40 @@ export function middleware(req: NextRequest, res: NextResponse) {
   const userStatus = req.cookies.get(USER_STATUS)?.value;
 
   if (!userStatus) {
-    // Todo: userStatus가 없으면 서버에서 받아오기
-    return response;
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/members/user-status`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+      const data = await res.json();
+
+      response.cookies.set({
+        name: USER_STATUS,
+        value: data.userStatus,
+        path: '/',
+      });
+
+      return response;
+    } catch (error) {
+      return response;
+    }
   }
+
+  console.log('set이 잘되었나?', req.cookies.get(USER_STATUS)?.value);
 
   if (path.startsWith('/chatroom') && userStatus === NON_COUPLE_USER) {
     return NextResponse.redirect(new URL('/onboarding', req.url));
   }
 
   if (path.startsWith('/onboarding') && userStatus !== NON_COUPLE_USER) {
+    console.log('onboarding에서 userStatus:', userStatus);
+
     return NextResponse.redirect(new URL('/chatroom', req.url));
   }
 
@@ -57,11 +82,3 @@ export const config = {
     '/status/:path*',
   ],
 };
-
-// cache? - token처럼 userStatus도 캐싱해야할까? - cookie
-// 저장 시점 - userStatus 받아올때마다 새로운 값을 저장하기 (로그인, 초대 수락, 초대링크 생성, chatroom 페이지 렌더링)
-// 저장 위치 - 쿠키
-
-// 저장된 값을 조회 - middleware에서 조회
-// 저장된 값이 없으면 - userStatus 받아오기
-// 저장된 값이 있으면 - userStatus 반환
