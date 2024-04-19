7ce8dbc039bc1eb7d83344577191bda4e13959b8
seul-dev <seul.dev@gmail.com>
2024-04-19 17:04:03 +0900

    Fix/routing error (#174)
    * feat: middleware 추가 - chechAuth 기능
    
    * remove: checkAuth 삭제
    
    * refactor: async 키워드 삭제
    
    * remove: 사용하지 않는 로컬스토리지 키 삭제
    
    * rename: 디렉토리 이름 변경(auth -> cookie)
    
    * feat: middleware - disallowRoute 기능 추가
    
    * refactor: getUserStatus 이름 변경
    
    * refactor: getUserStatus 이름 변경
    
    * feat: userStatus fetching 추가
    
    * remove: disallowAccess 함수 삭제
    - 서버사이드 라우트 가드 기능을 middleware에서 수행하기때문에 사용되지 않는 코드
    
    * remove: 불필요한 console.log 삭제
    
    * refactor: getUserStatus import
    
    * fix: userStatus update
    - 쿠키에 저장된 userStatus가 없을 경우에는 fetching한 값을 사용해서 redirect 처리하도록 수정
    
    * feat: accessToken 생성응답 코드 업데이트
    
    * feat: revalidateUserStatusCookie 추가
    
    * feat: revalidate 로직 수정
    
    * feat: middleware redirect 추가(my-info)
    
    * feat: login page userStatus 저장로직 추가
    
    * feat: checkAuthAndRedirect util 추가
    - middleware 삭제
    
    * fix: checkAuthAndRedirect 온보딩 페이지 리다이렉트 오류 수정
    
    * feat: chatroom redirect 추가
    - userStatus(서버에서 받아온)로 리다이렉트 되도록 수정

--- src/libs/serversideApi.ts
+++ src/libs/serversideApi.ts
@@ -1,4 +1,4 @@
-import { ACCESS_TOKEN } from '@/constants/auth';
+import { ACCESS_TOKEN } from '@/constants/cookie';
 import { ErrorResponse } from '@/types/api';
 import axios, {
   AxiosError,
@@ -93,7 +93,7 @@ async function refreshTokenAndRetryRequest(
   try {
     const res = await refreshInstance.post('/api/v1/members/auth/token');
 
-    if (res.status === 200) {
+    if (res.status === 200 || res.status === 201) {
       const accessToken = res.headers['authorization'].split(' ')[1];
 
       setServersideAccessToken(context, accessToken);
--- src/pages/onboarding/index.tsx
+++ src/pages/onboarding/index.tsx
@@ -10,7 +10,6 @@ import InviteStep from '@/components/onboarding/InviteStep';
 import useQuestionList from '@/hooks/queries/useQuestionList';
 import { createServerSideInstance, fetchData } from '@/libs/serversideApi';
 import type { Question } from '@/types/api';
-import { disallowAccess } from '@/util/disallowAccess';
 import SEO from '@/components/SEO/SEO';
 import OnboardingStepNavigator from '@/components/onboarding/OnboardingStepNavigator';
 import { checkAuthAndRedirect } from '@/util/checkAuthAndRedirect';
@@ -91,12 +90,6 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     queryKey: ['question-list'],
     queryFn: () => fetchData<Question[]>(api, '/api/v1/questions'),
   });
-
-  const redirection = await disallowAccess(context);
-
-  if (redirection) {
-    return redirection;
-  }
 
   return {
     props: {
